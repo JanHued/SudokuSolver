fb0e43fe5dea43727365959cf266f12f22b912f4
jan <jan.hued@gmx.de>
2022-02-08 05:51:26 -0800

    outsource solving algorithm and add unit test

--- app/src/main/java/com/example/sudokusolver/MainActivity.java
+++ app/src/main/java/com/example/sudokusolver/MainActivity.java
@@ -1,4 +1,4 @@
-qpackage com.example.sudokusolver;
+package com.example.sudokusolver;
 
 import android.annotation.SuppressLint;
 import android.content.res.Resources;
@@ -13,16 +13,20 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Solver solver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        solver = new Solver();
     }
 
     @SuppressLint("ResourceType")
     public void onClickSolve(View view) {
         List<Integer> grid = readInput();
-        solve(grid);
+        solver.solve(grid);
         writeOutput(grid);
     }
 
@@ -57,56 +61,5 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isNotANumber(String inputString) {
         return !List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9").contains(inputString);
-    }
-
-    private void solve(List<Integer> grid) {
-        for (int i = 0; i < grid.size(); i++) {
-            if (grid.get(i).equals(0)) {
-                for (int candidate = 1; candidate <= 9; candidate++) {
-                    if (possible(grid, i, candidate)) {
-                        grid.set(i, candidate);
-                        solve(grid);
-                        grid.set(i, 0);
-                    }
-                }
-            }
-        }
-    }
-
-    private boolean possible(List<Integer> grid, int index, int candidate) {
-        return possibleForRow(grid, index, candidate) && possibleForSquare(grid, index, candidate);
-    }
-
-    private boolean possibleForSquare(List<Integer> grid, int index, int candidate) {
-        int startRow = Math.floorDiv(getRowOfIndex(index), 3);
-        int startCol = Math.floorDiv(getColumnOfIndex(index), 3);
-        int startIndex = getIndexOfRowAndColumn(startRow, startCol);
-        return !getSublistOfSquareStartingAt(grid, startIndex).contains(candidate);
-    }
-
-    private int getRowOfIndex(int index) {
-        return Math.floorDiv(index, 9);
-    }
-
-    private int getColumnOfIndex(int index) {
-        return index - Math.floorDiv(index, 9) * 9;
-    }
-
-    private int getIndexOfRowAndColumn(int row, int column) {
-        return row * 9 + column;
-    }
-
-    private List<Integer> getSublistOfSquareStartingAt(List<Integer> grid, int index) {
-        List<Integer> sublistOfSquare = grid.subList(index, index + 2);
-        index += 9;
-        sublistOfSquare.addAll(grid.subList(index, index + 2));
-        index += 9;
-        sublistOfSquare.addAll(grid.subList(index, index + 2));
-        return sublistOfSquare;
-    }
-
-    private boolean possibleForRow(List<Integer> grid, int index, int candidate) {
-        int start = 9 * Math.floorDiv(index, 9);
-        return !grid.subList(start, start + 8).contains(candidate);
     }
 }
---
+++ app/src/main/java/com/example/sudokusolver/Solver.java
@@ -0,0 +1,82 @@
+package com.example.sudokusolver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solver {
+
+    protected void solve(List<Integer> grid) {
+        for (int i = 0; i < grid.size(); i++) {
+            if (grid.get(i).equals(0)) {
+                for (int candidate = 1; candidate <= 9; candidate++) {
+                    if (possible(grid, i, candidate)) {
+                        grid.set(i, candidate);
+                        solve(grid);
+                        if (!solved(grid)) {
+                            grid.set(i, 0);
+                        }
+                    }
+                }
+                return;
+            }
+        }
+    }
+
+    private boolean solved(List<Integer> grid) {
+        return !grid.contains(0);
+    }
+
+    private boolean possible(List<Integer> grid, int index, int candidate) {
+        return possibleForRows(grid, index, candidate) && possibleForSquare(grid, index, candidate);
+    }
+
+    private boolean possibleForRows(List<Integer> grid, int index, int candidate) {
+        return possibleForHorizontal(grid, index, candidate) && possibleForVertical(grid, index, candidate);
+    }
+
+    private boolean possibleForHorizontal(List<Integer> grid, int index, int candidate) {
+        int start = 9 * Math.floorDiv(index, 9);
+        return !grid.subList(start, start + 9).contains(candidate);
+    }
+
+    private boolean possibleForVertical(List<Integer> grid, int index, int candidate) {
+        int start = index % 9;
+        return !getVerticalRow(grid, start).contains(candidate);
+    }
+
+    private List<Integer> getVerticalRow(List<Integer> grid, int start) {
+        List<Integer> verticalRow = new ArrayList<>(grid.get(start));
+        for (int i = start + 9; i < grid.size(); i += 9) {
+            verticalRow.add(grid.get(i));
+        }
+        return verticalRow;
+    }
+
+    private boolean possibleForSquare(List<Integer> grid, int index, int candidate) {
+        int startRow = Math.floorDiv(getRowOfIndex(index), 3) * 3;
+        int startCol = Math.floorDiv(getColumnOfIndex(index), 3) * 3;
+        int startIndex = getIndexOfRowAndColumn(startRow, startCol);
+        return !getSublistOfSquareStartingAt(grid, startIndex).contains(candidate);
+    }
+
+    private int getRowOfIndex(int index) {
+        return Math.floorDiv(index, 9);
+    }
+
+    private int getColumnOfIndex(int index) {
+        return index - Math.floorDiv(index, 9) * 9;
+    }
+
+    private int getIndexOfRowAndColumn(int row, int column) {
+        return row * 9 + column;
+    }
+
+    private List<Integer> getSublistOfSquareStartingAt(List<Integer> grid, int index) {
+        List<Integer> sublistOfSquare = new ArrayList<>(grid.subList(index, index + 3));
+        index += 9;
+        sublistOfSquare.addAll(grid.subList(index, index + 3));
+        index += 9;
+        sublistOfSquare.addAll(grid.subList(index, index + 3));
+        return sublistOfSquare;
+    }
+}
